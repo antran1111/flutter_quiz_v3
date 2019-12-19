b3af70fd30990a6088758e1e5c31033ddb0f6063
antran1111 <54298061+antran1111@users.noreply.github.com>
2019-12-18 19:30:41 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -112,16 +112,7 @@ class _QuizPageState extends State<QuizPage> {
   int questionNumber = 0;
   QuizBank quizQuestions;
 
-  //loop through the quizQuestions
-/*
-  void loopQuiz(QuizBank quiz) {
-    //try loop when they click on it only
-    int totalquestions = quiz.allQuestions.length;
-    for (int i = 0; i < totalquestions; i++) {
-      print(quiz.allQuestions[i]);
-    }
-  }
-*/
+
   int incrementQuestionNumber(int q) {
     if (q > questions.length - 2) {
       q = 0;
@@ -244,8 +235,3 @@ class _QuizPageState extends State<QuizPage> {
   }
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
